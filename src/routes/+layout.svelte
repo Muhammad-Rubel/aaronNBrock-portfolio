0e9b65d59d211d31b5360c6f57169e0fe797f508
Muhammad-Rubel <rubel.contact@gmail.com>
2023-02-22 21:07:49 +0600

    Project created

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '$lib/css/tailwind.css';
+	// import Header from '$lib/components/Header.svelte';
+</script>
+
+<div class="relative">
+	<!-- <Header /> -->
+	<h1 class="text-4xl text-red-500">Test</h1>
+
+	<main>
+		<slot />
+	</main>
+
+	<!-- <Footer /> -->
+</div>
